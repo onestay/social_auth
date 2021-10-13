70c9e18229ac1e6a840bc06ba40bdd03f08a5e7d
Marius Meschter <17146830+onestay@users.noreply.github.com>
2021-10-13 13:33:48 +0200

    implement simple login functionality

--- social_auth/src/main.rs
+++ social_auth/src/main.rs
@@ -13,10 +13,11 @@ fn rocket() -> _ {
     let config = Config::from_env();
     let twitch = twitch_config::Twitch::new(config.twitch_client_id, config.twitch_client_secret, config.twitch_redirect_uri);
     let twitter = twitter_config::Twitter::new(config.twitter_api_key, config.twitter_api_secret, config.twitter_callback_url);
-
+    let sessions = templates::Sessions::new(config.password);
     rocket::build()
         .manage(twitch)
         .manage(twitter)
+        .manage(sessions)
         .mount("/", FileServer::from("public/"))
         .attach(templates::stage())
         .attach(twitch_config::stage())
@@ -30,7 +31,8 @@ struct Config {
     twitch_redirect_uri: String,
     twitter_api_key: String,
     twitter_api_secret: String,
-    twitter_callback_url: String
+    twitter_callback_url: String,
+    password: String
 }
 
 impl Config {
@@ -42,7 +44,7 @@ impl Config {
             twitter_api_key: env::var("TWITTER_API_KEY").expect("did not find a TWITTER_API_KEY"),
             twitter_api_secret: env::var("TWITTER_API_SECRET").expect("did not find a TWITTER_API_SECRET"),
             twitter_callback_url: env::var("TWITTER_CALLBACK_URL").unwrap_or_else(|_| String::from("http://127.0.0.1:8000/twitter/authorize/callback")),
-
+            password: env::var("AUTH_PASSWORD").expect("did not find a AUTH_PASSWORD")
         }
     }
 }
--- social_auth/src/templates.rs
+++ social_auth/src/templates.rs
@@ -1,13 +1,24 @@
 use crate::twitch_config::Twitch;
 use crate::twitter_config::Twitter;
-use rocket::{serde::Serialize, State};
+use rand::distributions::Alphanumeric;
+use rand::{thread_rng, Rng};
+use rocket::http::Cookie;
+use rocket::{
+    form::Form,
+    http::CookieJar,
+    request::{self, FromRequest, Request},
+    response::Redirect,
+    serde::Serialize,
+    State,
+};
 use rocket_dyn_templates::Template;
 use std::path::Path;
+use tokio::sync::Mutex;
 
 pub fn stage() -> rocket::fairing::AdHoc {
     rocket::fairing::AdHoc::on_ignite("templates", |rocket| async {
         rocket
-            .mount("/", routes![index])
+            .mount("/", routes![index, index_no_login, login, login_post])
             .attach(Template::fairing())
     })
 }
@@ -29,8 +40,52 @@ pub struct IndexContext {
     twitch_avail: bool,
 }
 
+pub struct Sessions {
+    sessions: Mutex<Vec<String>>,
+    password: String,
+}
+
+impl Sessions {
+    pub fn new(password: String) -> Self {
+        Sessions {
+            sessions: Mutex::new(vec![]),
+            password,
+        }
+    }
+}
+
+pub struct Authenticated;
+
+#[rocket::async_trait]
+impl<'r> FromRequest<'r> for Authenticated {
+    type Error = std::convert::Infallible;
+
+    async fn from_request(
+        request: &'r Request<'_>,
+    ) -> request::Outcome<Authenticated, Self::Error> {
+        if let Some(session_token) = request.cookies().get("session") {
+            let session_token = session_token.value();
+            let sessions = &request
+                .rocket()
+                .state::<Sessions>()
+                .expect("sessions state is not being managed")
+                .sessions
+                .lock()
+                .await;
+            if sessions.iter().any(|e| e == session_token) {
+                return request::Outcome::Success(Authenticated {});
+            }
+        };
+        request::Outcome::Forward(())
+    }
+}
+
 #[get("/")]
-async fn index(twitch: &State<Twitch>, twitter: &State<Twitter>) -> Template {
+async fn index(
+    twitch: &State<Twitch>,
+    twitter: &State<Twitter>,
+    _authenticated: Authenticated,
+) -> Template {
     let context = IndexContext {
         creator: "onestay".to_string(),
         twitch_url: twitch.get_authorize_url(),
@@ -42,4 +97,40 @@ async fn index(twitch: &State<Twitch>, twitter: &State<Twitter>) -> Template {
         twitter_avail: is_twitter_avail(),
     };
     Template::render("index", context)
+}
+
+#[get("/", rank = 2)]
+fn index_no_login() -> Redirect {
+    Redirect::to("/login")
+}
+
+#[get("/login")]
+async fn login() -> Template {
+    Template::render("login", ())
+}
+
+#[derive(FromForm)]
+struct LoginForm<'r> {
+    password: &'r str,
+}
+
+#[post("/login", data = "<login_form>")]
+async fn login_post(
+    login_form: Form<LoginForm<'_>>,
+    sessions: &State<Sessions>,
+    cookies: &CookieJar<'_>,
+) -> Redirect {
+    if login_form.password == sessions.password {
+        let session_token: String = thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(30)
+            .map(char::from)
+            .collect();
+
+        cookies.add(Cookie::new("session", session_token.clone()));
+        sessions.sessions.lock().await.push(session_token);
+        return Redirect::to("/");
+    }
+
+    Redirect::to("/login")
 }
